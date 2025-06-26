346821a567b62c7972049bb1023321b3f131c012
Siddhesh Pornima Ramesh Patole <siddpatole45@gmail.com>
2025-06-27 01:02:03 +0530

    Update page.tsx

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -11,32 +11,32 @@ export const projects: Project[] = [
   {
     title: "",
     description: "A technology company that builds economic infrastructure for the internet.",
-    src: "https://bharatkeveer.gov.in/media?imageId=42",
+    src: "https://bharatkeveer.gov.in/media?imageId=31",
   },
   {
     title: "",
     description: "A streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.",
-    src: "https://bharatkeveer.gov.in/media?imageId=34",
+    src: "https://bharatkeveer.gov.in/media?imageId=36",
   },
   {
     title: "",
     description: "A multinational technology company that specializes in Internet-related services and products.",
-    src: "https://bharatkeveer.gov.in/media?imageId=43",
+    src: "https://bharatkeveer.gov.in/media?imageId=37",
   },
   {
     title: "",
     description: "A technology company that focuses on building products that advance Facebook's mission of bringing the world closer together.",
-    src: "https://bharatkeveer.gov.in/media?imageId=41",
-  },
-  {
-    title: "",
-    description: "A multinational technology company focusing on e-commerce, cloud computing, digital streaming, and artificial intelligence.",
     src: "https://bharatkeveer.gov.in/media?imageId=38",
   },
   {
     title: "",
+    description: "A multinational technology company focusing on e-commerce, cloud computing, digital streaming, and artificial intelligence.",
+    src: "https://bharatkeveer.gov.in/media?imageId=39",
+  },
+  {
+    title: "",
     description: "A multinational technology company that develops, manufactures, licenses, supports, and sells computer software, consumer electronics, personal computers, and related services.",
-    src: "https://bharatkeveer.gov.in/media?imageId=57",
+    src: "https://bharatkeveer.gov.in/media?imageId=41",
   },
 ];
 
